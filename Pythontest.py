acd7cba03f74c9fc588a2fe3376ce6bea760cac6
AbishekDanny25 <abishekdannys@gmail.com>
2025-06-25 02:16:16 +0530

    Updated

--- Pythontest.py
+++ Pythontest.py
@@ -1,6 +1,14 @@
 #Nested Loop
 
-for i in range(1,3):
-    print("Week:",i)
-    for j in range(1,4):
-        print("Day:",j)
+# for i in range(1,3):
+#     print("Week:",i)
+#     for j in range(1,4):
+#         print("Day:",j)
+#---------------------------------------------------------
+#Star pattern
+
+for i in range(1,5):
+    print() 
+    for j in range(1,i+1):
+        print("*",end="")
+               
